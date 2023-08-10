d30c49dcf4facaf51466ad33da93325bc4944fdd
Tadeu Anderson <tadeuanderson15@hotmail.com>
2023-08-10 17:21:38 -0300

    Create variaveisEConstantes.js

---
+++ JAVASCRIPT/FUNDAMENTOS/variaveisEConstantes.js
@@ -0,0 +1,20 @@
+var a = 3
+//regra geral crie variáveis usando let
+//evite uso de var
+let b = 4
+ 
+var a = 30
+//let b = 40
+//não pode declarar duas vezes a mesma declaração/ já o var aceita fazendo uma nova declaração.
+b = 40
+
+a = 300
+b = 400
+
+console.log(a,b)
+
+const c = 5
+//constante não pode receber atribuição
+//c = 50
+
+console.log(c)
